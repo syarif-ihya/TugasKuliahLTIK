12ab3ea7ec561ca301cc3cd1254f3252b2797c07
syarif-ihya <syarifihya17@gmail.com>
2025-11-06 21:26:00 +0700

    First Commit

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("tahap 1 - membuat repo github ")
+print("tahap 1 - membuat folder proyek ")
+print("tahap 2 - membuat repo github ")
+print("tahap 3 - menghubungkan repo github ")
+print("tahap 4 - menambahkan dan mencommit untuk mempushnya ke github ")
